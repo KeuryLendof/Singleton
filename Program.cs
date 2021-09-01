7f0aaef008819da54aeaf777f3b26522744aa90e
KeuryLendof <lendofkeury@gmail.com>
2021-08-31 20:36:46 -0400

    'version'

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Configure # de la Central: ");//Se solicita un # telefonico
+            int numeroTelefonico = int.Parse(Console.ReadLine()); //Se captura lo ingresado por el usuario
+
+            
+            Telefono telefono1 = Telefono.GetTelefono(numeroTelefonico); 
+
+            //Se muestra el # telefonico configurado
+            telefono1.Mostrar();
+
+            Console.ReadKey();
+
+            Console.WriteLine("Configure el otro # de la Central: ");
+            int otroNumero = int.Parse(Console.ReadLine());
+
+            Telefono telefono2 = Telefono.GetTelefono(otroNumero);
+
+            telefono1.Mostrar();
+            telefono2.Mostrar();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Telefono.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Telefono
+    {
+        private static Telefono telefono;//atributo que representa la instancia de la clase
+        
+        private int numero; // atributo para contener el # de telefono
+
+        //Metodo constructor publico
+        private Telefono(int numero)
+        {
+            this.numero = numero;
+        }
+
+        public static Telefono GetTelefono(int num)
+        {
+            if(telefono==null)
+            {
+                telefono = new Telefono(num);
+            }
+            return telefono;
+        }
+        
+        public void Mostrar()
+        {
+            Console.WriteLine($"El numero de la central configurado es: {numero}");
+        }
+            
+       
+    }
+}
